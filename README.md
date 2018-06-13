98aecd3effd515d09c7a3ec43fed74d85d3e839b
yangyuji <394712273@qq.com>
2018-06-13 10:37:52 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hammer swipe tabs
-仿电商商品详情页的左右切换tab效果，使用hammer.js实现的，跟原生的区别是hammer.js做了pan和swipe的封装，可以避免拖动的时候出现上下左右一起动情况。
+a tabs script depends on hammer.js, swipe action change the tab.
 
-## 预览
-> * 页面[点击这里](https://yangyuji.github.io/hammer-swipe-tabs/demo.html)
-> * ![扫描二维码](https://github.com/yangyuji/hammer-swipe-tabs/blob/master/img/qrcode.png)
+## preview
+> * page [click here](https://yangyuji.github.io/hammer-swipe-tabs/demo.html)
+> * ![qrcode](https://github.com/yangyuji/hammer-swipe-tabs/blob/master/img/qrcode.png)
--- hammerSwipeTabs.js
+++ hammerSwipeTabs.js
@@ -2,8 +2,8 @@
 * author: "oujizeng",
 * license: "MIT",
 * github: "https://github.com/yangyuji/hammer-swipe-tabs",
-* name: "hammerSwipePanel.js",
-* version: "1.1.0"
+* name: "hammerSwipeTabs.js",
+* version: "2.0.0"
 */
 
 (function (root, factory) {
@@ -27,6 +27,19 @@
         });
     }
 
+    var _transitionEnd = function (el, fun) {
+        var vendors = ['webitTransitionEnd', 'transitionend'];
+        var handler = function (e) {
+            [].forEach.call(vendors, function (vendor) {
+                el.removeEventListener(vendor, handler, false);
+            });
+            fun.apply(el, arguments);
+        };
+        [].forEach.call(vendors, function (vendor) {
+            el.addEventListener(vendor, handler, false);
+        });
+    }
+
     var hammerSwipeTabs = {
 
         init: function(opt) {
@@ -35,7 +48,6 @@
                 clientWidth = window.screen.width,
                 clientHeight = window.screen.height,
                 panelNum = 3,                               // 默认屏个数
-                isSwipe = false,                            // 是否是swipe动作
                 currentPanel = 1,                           // 当前所在屏
                 currentMoveX = 0,                           // 横向动画已经滑动距离
                 //currentMoveY = 0,                         // 竖向动画已经滑动距离
@@ -43,110 +55,69 @@
                 scroller = document.querySelector(opt.scroller);   // 滚动容器
 
             var hammer = new Hammer(document.querySelector(opt.page));
-            // 防止panstart不出现和干扰上下滚动的问题
             hammer.get('pan').set({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 0 });
 
-            hammer.on('panstart panleft panright pancancel swipe panend', function(ev) {
+            hammer.on('swipe swipeleft swiperight', function(ev) {
                 console.log(ev.type);
-                if (ev.type === 'panstart') {
+                if (ev.type === 'swipe') {
                     _translate(scroller, 'TransitionProperty', 'transform');
                     _translate(scroller, 'TransitionTimingFunction', 'cubic-bezier(0, 0, 0.25, 1)');
-                    _translate(scroller, 'TransitionDuration', '0ms');
+                    _translate(scroller, 'TransitionDuration', '350ms');
                 }
 
-                // 向右滑动
-                if (ev.type == 'panright') {
-                    // 已经到最左边了
+                // 向右滑动一屏
+                if (ev.type == 'swiperight') {
+                    // 已经到最左边了，来一点动画
                     if (currentMoveX === 0) {
-                        // 加上阻尼效果
-                        // var x = ev.deltaX > clientWidth ? clientWidth * coefficient : ev.deltaX * coefficient;
-                        // 改为不限制滚动距离
                         var x = ev.deltaX * coefficient;
                         _translate(scroller, 'Transform', 'translateX(' + x + 'px)');
+                        _transitionEnd(scroller, function (e) {
+                            _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
+                        });
+                        return;
                     } else {
-                        _translate(scroller, 'Transform', 'translateX(' + (currentMoveX + Math.abs(ev.deltaX)) + 'px)');
+                        // 向右滑一屏
+                        currentPanel -= 1;
+                        currentMoveX += clientWidth;
+                        _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
+                        // 配置panel的高度，避免被高的撑开
+                        setTimeout(function () {
+                            for (var n = 0; n < scroller.children.length; n++) {
+                                if (n === currentPanel - 1) {
+                                    scroller.children[n].style.height = 'auto';
+                                } else {
+                                    scroller.children[n].style.height = clientHeight + 'px';
+                                }
+                            }
+                        }, 0);
                     }
                 }
 
-                // 向左滑动
-                if (ev.type == 'panleft') {
-                    // 已经到最右边了
+                // 向左滑动一屏
+                if (ev.type == 'swipeleft') {
+                    // 已经到最右边了，来一点动画
                     if (Math.abs(currentMoveX) >= (panelNum - 1) * clientWidth) {
-                        // 加上阻尼效果
-                        // var x = Math.abs(ev.deltaX) > clientWidth ? currentMoveX - (clientWidth * coefficient) : currentMoveX - (Math.abs(ev.deltaX) * coefficient);
-                        // 改为不限制滚动距离
                         var x = currentMoveX - (Math.abs(ev.deltaX) * coefficient);
                         _translate(scroller, 'Transform', 'translateX(' + x + 'px)');
+                        _transitionEnd(scroller, function (e) {
+                            _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
+                        });
+                        return;
                     } else {
-                        _translate(scroller, 'Transform', 'translateX(' + (currentMoveX - Math.abs(ev.deltaX)) + 'px)');
-                    }
-                }
-
-                // swipe行为
-                if (ev.type === 'swipe') {
-                    // 因为swipe结束后肯定会触发panend，所以这个地方不做处理，统一放到panend里面做
-                    isSwipe = true;
-                }
-
-                // 处理pancancel的情况
-                if (ev.type === 'pancancel') {
-                    _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
-                }
-
-                if (ev.type === 'panend') {
-
-                    // 放慢动画，避免卡
-                    _translate(scroller, 'TransitionDelay', '0ms');
-                    _translate(scroller, 'TransitionDuration', '350ms');
-
-                    if (isSwipe) {
-                        if (ev.deltaX < 0) {
-                            // 已经是最后一屏
-                            if (Math.abs(currentMoveX) == (panelNum - 1) * clientWidth) {
-                                _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
-                            } else {
-                                // 向左滑一屏
-                                currentPanel += 1;
-                                currentMoveX -= clientWidth;
-                                _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
-                                // 配置panel的高度，避免被高的撑开
-                                setTimeout(function () {
-                                    for (var n = 0; n < scroller.children.length; n++) {
-                                        if (n === currentPanel - 1) {
-                                            scroller.children[n].style.height = 'auto';
-                                        } else {
-                                            scroller.children[n].style.height = clientHeight + 'px';
-                                        }
-                                    }
-                                }, 0);
-                            }
-                        } else {
-                            // 当前是第一屏
-                            if (currentMoveX === 0) {
-                                _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
-                            } else {
-                                // 向右滑一屏
-                                currentPanel -= 1;
-                                currentMoveX += clientWidth;
-                                _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
-                                // 配置panel的高度，避免被高的撑开
-                                setTimeout(function () {
-                                    for (var n = 0; n < scroller.children.length; n++) {
-                                        if (n === currentPanel - 1) {
-                                            scroller.children[n].style.height = 'auto';
-                                        } else {
-                                            scroller.children[n].style.height = clientHeight + 'px';
-                                        }
-                                    }
-                                }, 0);
-                            }
-                        }
-
-                        // 还原参数
-                        isSwipe = false;
-
-                    } else {
+                        // 向左滑一屏
+                        currentPanel += 1;
+                        currentMoveX -= clientWidth;
                         _translate(scroller, 'Transform', 'translateX(' + currentMoveX + 'px)');
+                        // 配置panel的高度，避免被高的撑开
+                        setTimeout(function () {
+                            for (var n = 0; n < scroller.children.length; n++) {
+                                if (n === currentPanel - 1) {
+                                    scroller.children[n].style.height = 'auto';
+                                } else {
+                                    scroller.children[n].style.height = clientHeight + 'px';
+                                }
+                            }
+                        }, 0);
                     }
                 }
             });
